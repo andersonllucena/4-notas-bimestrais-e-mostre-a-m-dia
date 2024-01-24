53fbaa5333f05f77280f983f5048006852d41e93
Anderson Lucena <obeloanarquista@gmail.com>
2024-01-24 06:08:41 -0300

    Add files via upload

---
+++ solicitar_as_4_notas_bimestrais.py
@@ -0,0 +1,11 @@
+# Solicitar as 4 notas bimestrais
+nota1 = float(input("Digite a nota do 1º bimestre: "))
+nota2 = float(input("Digite a nota do 2º bimestre: "))
+nota3 = float(input("Digite a nota do 3º bimestre: "))
+nota4 = float(input("Digite a nota do 4º bimestre: "))
+
+# Calcular a média
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+# Mostrar a média
+print(f"A média das notas é: {media}")
